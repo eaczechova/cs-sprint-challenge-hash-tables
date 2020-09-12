25c9f9c685b41d3e4024705d2e67b3c26481d36b
eaczechova <ewa.czech@hotmail.com>
2020-09-12 18:33:25 +0200

    add first passing solution to ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
 
+    for i in range(0, length):
+        for j in range(i+1, length):
+            if weights[i] + weights[j] == limit:
+                result.append(j)
+                result.append(i)
+                return result
     return None
+
+
+# to store 
