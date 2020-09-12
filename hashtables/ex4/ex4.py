11de42a9d8ac900723f006ab988a1d366848b8fe
eaczechova <ewa.czech@hotmail.com>
2020-09-12 19:12:57 +0200

    add solution for ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    for number in a:
+        if number > 0 and number not in cache:
+            cache[number] = 0
+      
+    for neg_num in a:
+        if neg_num < 0 and (neg_num * -1) in cache:
+            cache[neg_num * -1] += 1
 
+    for (key, value) in cache.items():
+        if value >= 1:
+            result.append(key)
     return result
+   
 
 
 if __name__ == "__main__":
