6c385892647c8f7e0326abdf7a94c21341098937
eaczechova <ewa.czech@hotmail.com>
2020-09-14 22:57:22 +0200

    add solution to ex5

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,14 +4,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
     result = []
-
+    cache = dict()
+   
     for i in range(0, length):
         for j in range(i+1, length):
             if weights[i] + weights[j] == limit:
                 result.append(j)
                 result.append(i)
                 return result
+          
     return None
+   
 
+    
 
-# to store 
+    # for index, weight in enumerate(weights):
+    #     result = limit - weights[weight]
+
+    #     if result in cache:
+    #         return [cache[result], index]
+    #     else:
+    #         cache[result] = index
+
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,33 @@
 # Your code here
 
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
+    cache = {}
+    result = []
     # Your code here
+    for f in files:
+        #split path using "/" and assign last item of the list
+        path = f.split("/")[-1]
+        print("path", path)
+        # if file not in dict create key and assign value of list with matching path
+        if path not in cache:
+            cache[path] = []
+            cache[path].append(f)
+        # if file not in dict add anothe path to the list
+        else: 
+            cache[path].append(f)
 
+    for q in queries:
+        if q in cache:
+            result.extend(cache[q])
     return result
+               
+          
+                    
+
+
 
 
 if __name__ == "__main__":
